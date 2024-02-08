4faa78bef22d030f607fd5713940a6cd2983440c
mario-dg <mario.dagraca@gmx.com>
2024-02-08 16:52:33 +0100

    Add Firebase plugins and global color constants

---
+++ lib/globals.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColors = Color.fromRGBO(25, 25, 25, 255);
+const webBackgroundColors = Color.fromRGBO(45, 45, 45, 255);
+const mobileSearchColor = Color.fromRGBO(55, 55, 55, 255);
+
+const primaryColor = Colors.white;
+const secondaryColor = Colors.grey;
+const accentColor = Colors.blueAccent;
